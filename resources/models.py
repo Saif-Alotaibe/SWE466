412ba5ef511faec9239d7a77cc5734ad8d808c4a
Saif_Alotaibe <safe1otb@gmail.com>
2021-03-20 15:16:21 +0300

    Add resource model

--- resources/models.py
+++ resources/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from commons.models import Model
 
-# Create your models here.
+
+class Resource(Model):
+    name = models.CharField(max_length=70)
+    resource_type = models.CharField(max_length=70)  # Todo: check for all possible values and convert it to list
+    material = models.CharField(max_length=70)  # Todo: check whether it's list or not
+    max_number_of_resources = models.FloatField()
+    '''
+    1-Check for many-to-many with task
+    2- Check for st.resource and add it
+    3- Check for ovt and add it
+    4- Check for cost/use and add it
+    '''
