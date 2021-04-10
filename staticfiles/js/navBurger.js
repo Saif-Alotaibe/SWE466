b18281c8c6d4518a22304b8a272e55a19c37f4e6
Saif_Alotaibe <safe1otb@gmail.com>
2021-04-10 04:41:10 +0300

    Add function to calculate duration

--- staticfiles/js/navBurger.js
+++ staticfiles/js/navBurger.js
@@ -8,4 +8,20 @@ $(document).ready(function() {
         $(".navbar-menu").toggleClass("is-active");
   
     });
+
+     $("#finish").change(function () {
+
+     let start_date = $("#start").val();
+     let end_date = $("#finish").val();
+     if (start_date.length >0 ){
+          const date1 = new Date(start_date);
+          const date2 = new Date(end_date);
+          const diffTime = Math.abs(date2 - date1);
+          const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1;
+          $("#dur").val(diffDays);
+     }
+
+
+  });
+
   });
